8d1247d88744ea1ae6133c59d4eb96369768a680
19002090213 <1049697743@qq.com>
2020-03-13 11:00:51 +0800

    demo

--- stat.js
+++ stat.js
@@ -1,14 +1,20 @@
-function reduce(array, combine, start) {
+(function(){
+    function reduce(array, combine, start) {
     var current = start;
         for (var element of array) {
-            current = combine(current, element);
-               }return current;
+        current = combine(current, element);
+            }
+            return current;
 } 
-
 var max=function(data){
     return  reduce(
         data,
         function(a,b){
-            return a<b.credit?a:b.credit;
-        },10)
+            return a>b.credit?a:b.credit;
+        },0)
+    
 }
+window.my={"max":max}
+})()
+
+
